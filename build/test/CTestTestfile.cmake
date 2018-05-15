ba9d301fa9a51eef2876734225f29cf01cfa0c57
Jeshwanth Challagundla <jeswanth111@gmail.com>
2018-05-15 18:33:17 -0400

    remove build directory

--- build/test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cz0j2j/cplusplus-libraries/test
-# Build directory: /home/cz0j2j/cplusplus-libraries/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
